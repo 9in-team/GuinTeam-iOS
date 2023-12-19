68bfe30c12baae66fd5a0060f0d8667690088a1f
HeonJin Ha <weizen511@gmail.com>
2023-12-19 22:45:41 +0900

    Initial Commit

---
+++ GuinTeam-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuinTeam-iOS
+//
+//  Created by HeonJin Ha on 12/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
